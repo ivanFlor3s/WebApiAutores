c3fd031cc64273ea1ccf2d4134e8779872fdace7
ivanFlor3s <ifloresmanc96@gmail.com>
2023-03-22 00:07:23 -0300

    Creo mi clase startup

--- WebApiAutores/Program.cs
+++ WebApiAutores/Program.cs
@@ -1,25 +1,14 @@
+using WebApiAutores;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startUp = new StartUp(builder.Configuration);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+startUp.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startUp.Configure(app, app.Environment);
 
 app.Run();

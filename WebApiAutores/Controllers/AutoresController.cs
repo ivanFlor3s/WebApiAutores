d794080fc1fa072f35d61072cd8b3a1a94c192e4
ivanFlor3s <ifloresmanc96@gmail.com>
2023-03-22 23:39:48 -0300

    Autores entities+ limpio weather controller

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get() 
+        {
+            return new List<Autor>()
+            {
+                new Autor(){Id = 1, Name = "buLLIERSE"},
+                new Autor(){Id = 2, Name = "CILERIA CILERIA"},
+            };
+        }
+    }
+}
---
+++ WebApiAutores/Entities/Autor.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiAutores.Entities
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
